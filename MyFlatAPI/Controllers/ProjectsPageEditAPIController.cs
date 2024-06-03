6732c6026598e813f7dde7830294adef52e41fca
AndrewKooklin <bolt999@yandex.ru>
2024-06-03 17:19:37 +0300

    Add ProjectsPageEditAPIController

---
+++ MyFlatAPI/Controllers/ProjectsPageEditAPIController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFlatAPI.Data;
+
+namespace MyFlatAPI.Controllers
+{
+    public class ProjectsPageEditAPIController : Controller
+    {
+        private DataManager _dataManager;
+
+        public ProjectsPageEditAPIController(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
