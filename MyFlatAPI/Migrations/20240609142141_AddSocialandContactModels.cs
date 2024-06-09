02e88e3f906a9dcffe19c9e6bc1d3bc0d23238f1
AndrewKooklin <bolt999@yandex.ru>
2024-06-09 17:23:42 +0300

    Add Migration AddSocialandContactModels

---
+++ MyFlatAPI/Migrations/20240609142141_AddSocialandContactModels.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyFlatAPI.Migrations
+{
+    public partial class AddSocialandContactModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "292f5bd7-14a9-476a-8d99-a0d9cb29bd5c");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f614dd2f-379f-4458-aaaf-c23b17dd4c4e");
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactAddress = table.Column<string>(nullable: true),
+                    ContactPhone = table.Column<string>(nullable: true),
+                    ContactEmail = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SocialLinks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SocialImage = table.Column<byte[]>(nullable: true),
+                    SocialLink = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocialLinks", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "3a8c1060-a260-4901-8fd0-e2d29668cd61", "6d5c54e4-667f-4b61-9ac9-d61039cdf950", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "9a327ced-f16b-46eb-a9e6-eb5233ce0b0e", "302dc497-bf7b-49fa-af47-cc1123b3fe8e", "User", "USER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "SocialLinks");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3a8c1060-a260-4901-8fd0-e2d29668cd61");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9a327ced-f16b-46eb-a9e6-eb5233ce0b0e");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f614dd2f-379f-4458-aaaf-c23b17dd4c4e", "6d5c54e4-667f-4b61-9ac9-d61039cdf950", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "292f5bd7-14a9-476a-8d99-a0d9cb29bd5c", "302dc497-bf7b-49fa-af47-cc1123b3fe8e", "User", "USER" });
+        }
+    }
+}
